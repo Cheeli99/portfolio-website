27c3eaee6b52f474cdd477ecaddf510a2812edc6
cheeli99 <ejmccarthy474@protonmail.com>
2022-12-28 12:08:56 -0600

    added about section

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,9 +8,18 @@ const Body = () => {
     return (
         <div>
             <main role="main" class="inner cover">
-                <h1 class="cover-heading">Projects</h1>
 
-                <section id="Games" class="Games">
+                <header class="masthead text-white text-center">
+                <div class="heading-container d-flex align-items-center flex-column">
+                    
+                    <h1 class="masthead-heading mb-0">Evan McCarthy</h1>
+                    
+                    <p class="masthead-subheading font-weight-light mb-0">Front End Developer - Software Developer</p>
+                </div>
+                </header>
+
+                <section id="portfolio" class="portfolio">
+                <h1 class="cover-heading">Projects</h1>
                     <div class="container">
                         <div class="row">
                             <div class="col-lg mb-4 mb-lg-5">
@@ -33,7 +42,7 @@ const Body = () => {
                                             <img src={require("../img/kiosk_first_screen.png")} class="rounded box-img img-fluid mx-3 d-block" alt="Math tutor program"></img>
                                         </a>
                                         <h2 class="icon-text-heading">Center for Applied Technology (CAT) Kiosk Program</h2>
-                                        <p class="icon-text">Created with Java and Javafx, this seminar project provides information about the CAT building.</p>
+                                        <p class="icon-text">Created with Java and Javafx, this seminar project provides information about Wayne State College's CAT building.</p>
                                     </p>
                                 </div>
                             </div>
@@ -53,6 +62,55 @@ const Body = () => {
                         </div>
                     </div>
                 </section>
+                
+                <section class="about page-section text-white mb-0" id="about">
+
+                <h1 class="page-section-heading text-center text-uppercase text-white mb-5">About</h1>
+
+                <div class="about__content">
+
+                    <div class="profile">
+                        <div class="profile_blurb"><p class="lead"></p>A recent grad from Wayne State College, I'm a front end developer and software developer with a deep 
+                                passion for learning and growing. The combination of problem solving, creativity and logic, drives my excitement and passion for web development and software development.
+                                When I'm not programming, I like to spend my time playing video games, keeping fit and producing YouTube videos.
+                        </div>
+                    </div>
+
+                    <div class="skills">
+                        <div class="skills__row">
+                            
+                            <div class="skills__item">
+                            <img src={require("../img/html_logo.png")} class="" alt="HTML logo"></img>
+                            </div>
+                            <div class="skills__item">
+                            <img src={require("../img/css_logo.png")} class="" alt="CSS logo"></img>
+                            </div>
+                        </div>
+                        <div class="skills__row">
+                            <div class="skills__item">
+                            <img src={require("../img/js_logo.png")} class="" alt="Javascript logo"></img>
+                            </div>
+                            <div class="skills__item">
+                            <img src={require("../img/git_logo.png")} class="" alt="Git logo"></img>
+                            </div>
+                            <div class="skills__item">
+                            <img src={require("../img/react_logo.png")} class="" alt="React.js logo"></img>
+                            </div>
+                        </div>
+                        <div class="skills__row">
+                            <div class="skills__item">
+                            <img src={require("../img/java_logo.png")} class="" alt="Java logo"></img>
+                            </div>
+                            <div class="skills__item">
+                            <img src={require("../img/mysql_logo.png")} class="" alt="React.js logo"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </section>
+
+                
+
             </main>
         </div>
     )
