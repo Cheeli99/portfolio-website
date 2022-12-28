2ebb1e29de5d29de2e162a548594002f3208a447
cheeli99 <ejmccarthy474@protonmail.com>
2022-12-28 12:10:06 -0600

    changed links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ const Navbar = () => {
     <div>
       <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand">Evan McCarthy</h3>
+          <h3 class="masthead-brand" href="#page-top">Evan McCarthy</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="index.html">Home</a>
-            <a class="nav-link" href="privacy.html">Privacy Policy</a>
-            <a class="nav-link" href="credits.html">Credits</a>
+            <a class="nav-link active" href="#portfolio">Portfolio</a>
+            <a class="nav-link" href="#about">About</a>
+            <a class="nav-link" href="#contact">Contact</a>
           </nav>
         </div>
       </header>
